Longtail CUB and ImageNet-R datasets created. Corresponding dataloaders modified. MOS file updated with itreations logging. NaN covariance matrix issue fixed for LT scenario. .env created to point to local dataset repositories. This will be removed later for keeping directories anonymous.

utils/data_manager.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iCIFAR224, iImageNetR,iImageNetA,CUB, objectnet, omnibenchmark, vtab
+from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iCIFAR224, iImageNetR, iImageNetR_Longtail, iImageNetA, CUB, CUB_Longtail, objectnet, omnibenchmark, vtab
 
 
 class DataManager(object):
@@ -227,10 +227,14 @@ def _get_idata(dataset_name, args=None):
         return iCIFAR224(args)
     elif name == "imagenetr":
         return iImageNetR(args)
+    elif name == "imagenetr_lt":
+        return iImageNetR_Longtail(args)
     elif name == "imageneta":
         return iImageNetA()
     elif name == "cub":
         return CUB()
+    elif name == "cub_lt":
+        return CUB_Longtail()
     elif name == "objectnet":
         return objectnet()
     elif name == "omnibenchmark":